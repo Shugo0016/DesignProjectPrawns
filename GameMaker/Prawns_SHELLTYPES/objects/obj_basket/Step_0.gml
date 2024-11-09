bbe78b433c87988ae9759b9ed2470bd9e196780a
santosabby84 <159199448+abbys4@users.noreply.github.com>
2024-11-09 11:35:03 -0500

    Establish connection between Gamemaker data and Postgresql

--- GameMaker/Prawns_SHELLTYPES/objects/obj_basket/Step_0.gml
+++ GameMaker/Prawns_SHELLTYPES/objects/obj_basket/Step_0.gml
@@ -3,7 +3,7 @@
 	x = obj_player.x
 	y = obj_player.y - 50;
 	
-	if keyboard_check_pressed(vk_space) or mouse_check_button_pressed(right) {
+	if keyboard_check_pressed(vk_space)  {
 		if collect_type == "int" { collect_type = "float" }
 		else if collect_type == "float" { collect_type = "string" }
 		else if collect_type == "string" { collect_type = "bool" }
---
+++ server.js
@@ -0,0 +1,61 @@
+//Connect PostgreSQL with Gamemaker 
+//TODO: edit to fit with current postgresql tables/data format
+
+const express = require('express');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3030;
+
+// PostgreSQL connection pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'demo',
+    password: 'marbby84',
+    port: 5433,
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+// Receive data from Gamemaker
+/* - In GameMaker obj_data_manager Create -
+   --> http_request(http://localhost:3030/data, "POST", ds_map(Content-Type "application/json"), ds_map(my_id,myscore))
+    --> May change my_id to come from database instead of game
+*/
+app.post('/data', async (req, res) => {
+    const { data_field } = req.body;
+   // console.log(req.body); // Log the entire body
+    const { my_id, my_score } = req.body;
+    console.log(`id: ${my_id}, score: ${my_score}`); // Check if values are received
+    console.log(my_id,my_score)
+    res.send([my_id,my_score]);
+    try {
+        const result = await pool.query('INSERT INTO gm2tests (my_id, my_score) VALUES ($1, $2)', [my_id, my_score]);
+     
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error saving data');
+    }
+});
+
+app.get('/data', async (req, res) => {
+    try {
+        //display
+        const result = await pool.query('SELECT * FROM gm2tests');
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error executing query', err);
+        res.status(500).send('Server error');
+    }
+});
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
